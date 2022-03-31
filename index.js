e4f5bca905ec0c2beb49916fa5ae084d483b21a4
Genival Dantas <genineto10@gmail.com>
2022-03-31 20:36:27 -0300

    carregando js limpo

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-let titulo = document.querySelector('h1')
-let instrucoes = document.querySelector('#instrucoes')
-let aviso = document.querySelector('#aviso')
-
+let titulo = document.querySelector('h1');
+let instrucoes = document.querySelector('#instrucoes');
+let aviso = document.querySelector('#aviso');
 
 // PERGUNTA
-let numQuestao = document.querySelector('#numQuestao')
-let pergunta   = document.querySelector('#pergunta')
+let numQuestao = document.querySelector('#numQuestao');
+let pergunta   = document.querySelector('#pergunta');
 
 // ALTERNATIVAS
-let a = document.querySelector('#a')
-let b = document.querySelector('#b')
-let c = document.querySelector('#c')
-let d = document.querySelector('#d')
+let a = document.querySelector('#a');
+let b = document.querySelector('#b');
+let c = document.querySelector('#c');
+let d = document.querySelector('#d');
 
 // article com a class questoes
-let articleQuestoes = document.querySelector('.questoes')
+let articleQuestoes = document.querySelector('.questoes');
 
 // ol li com as alternativas
-let alternativas = document.querySelector('#alternativas')
+let alternativas = document.querySelector('#alternativas');
 
 // Jogadores
 let divJogador1 = document.querySelector('.jogador1');
@@ -30,21 +29,21 @@ let botao_j1 = document.querySelector('.botao_j1');
 let botao_j2 = document.querySelector('.botao_j2');
 
 // configura botões
-let clicou = ''
+let clicou = '';
 
 // // 1
 botao_j1.addEventListener('click', () => {
-    instrucoes.textContent = `${jogador1.textContent} joga!`
-    clicou = 'jogador1'
+    instrucoes.textContent = `${jogador1.textContent} joga!`;
+    clicou = 'jogador1';
     botao_j2.disabled = true;
-    clicouPrajogar = true
+    clicouPrajogar = true;
 });
 // // 2
 botao_j2.addEventListener('click', () => {
-    instrucoes.textContent = `${jogador2.textContent} joga!`
-    clicou = 'jogador2'
+    instrucoes.textContent = `${jogador2.textContent} joga!`;
+    clicou = 'jogador2';
     botao_j1.disabled = true;
-    clicouPrajogar = true
+    clicouPrajogar = true;
 });
 
 // Pontos jogadores
@@ -52,17 +51,6 @@ let pontos_j1 = document.querySelector('.pontos_1');
 let pontos_j1_num = 0;
 let pontos_j2 = document.querySelector('.pontos_2');
 let pontos_j2_num = 0;
-
-
-// Botão jogar
-const botaoJogar = document.querySelector('.botao_jogar');
-botaoJogar.addEventListener('click', () => {
-    if (botaoJogar.disabled === true) {
-        console.log('função bloqueada');
-    } else {
-        console.log('botão jogar');
-    }
-})
 
 // Questões
 const q0 = {
@@ -73,7 +61,7 @@ const q0 = {
     alternativaC : "Alternativa C",
     alternativaD : "Alternativa D",
     correta      : "0",
-}
+};
 
 const q1 = {
     numQuestao   : 1,
@@ -83,7 +71,7 @@ const q1 = {
     alternativaC : "Epiteto",
     alternativaD : "Sócrates",
     correta      : "Diógenes",
-}
+};
 
 const q2 = {
     numQuestao   : 2,
@@ -93,7 +81,7 @@ const q2 = {
     alternativaC : "Getúlio Vargas",
     alternativaD : "João Goulart",
     correta      : "João Goulart",
-}
+};
 
 const q3 = {
     numQuestao   : 3,
@@ -103,7 +91,7 @@ const q3 = {
     alternativaC : "Montesquieu",
     alternativaD : "Thomas Hobbes",
     correta      : "Maquiavel",
-}
+};
 
 const q4 = {
     numQuestao   : 4,
@@ -113,7 +101,7 @@ const q4 = {
     alternativaC : "Bram Stoker",
     alternativaD : "Ian Fleming",
     correta      : "Mary Ann Shelly",
-}
+};
 
 const q5 = {
     numQuestao   : 5,
@@ -123,7 +111,7 @@ const q5 = {
     alternativaC : "1920",
     alternativaD : "1914",
     correta      : "1914",
-}
+};
 
 const q6 = {
     numQuestao   : 6,
@@ -133,7 +121,7 @@ const q6 = {
     alternativaC : "Van Gogh",
     alternativaD : "Tarsila do Amaral",
     correta      : "Van Gogh",
-}
+};
 
 const q7 = {
     numQuestao   : 7,
@@ -143,7 +131,7 @@ const q7 = {
     alternativaC : "Zeus e Hades",
     alternativaD : "Dionísio e Deméter",
     correta      : "Zeus e Hades",
-}
+};
 
 // Jogadores
 const j1 = {
@@ -152,7 +140,7 @@ const j1 = {
     sobreome: "Júnior",
     email: "neyjunior@gmail.com",
     idade: "33"
-}
+};
 
 const j2 = {
     id: "1M35",
@@ -160,7 +148,7 @@ const j2 = {
     sobreome: "Lionel",
     email: "messiLio@gmail.com",
     idade: "35"
-}
+};
 
 // Constante com uma arrau de objetos com as questões
 const questoes = [q0, q1, q2, q3, q4, q5, q6, q7];
@@ -172,40 +160,40 @@ const jogadores = [j1, j2];
 let numero = document.querySelector('#numero');
 
 // total
-let total  = document.querySelector('#total')
+let total  = document.querySelector('#total');
 
 // Coloca um numero "1" de 7
-numero.textContent = q1.numQuestao
+numero.textContent = q1.numQuestao;
 
 // Coloca o número 1 de "7"
-let totalDeQuestoes = (questoes.length)-1
-console.log("Total de questões " + totalDeQuestoes)
-total.textContent = totalDeQuestoes
+let totalDeQuestoes = (questoes.length)-1;
+console.log("Total de questões " + totalDeQuestoes);
+total.textContent = totalDeQuestoes;
 
 // Monta o jogador1 
-jogador1.textContent = j1.nome
+jogador1.textContent = j1.nome;
 pontos_j1.textContent = 'Pontos: ' + pontos_j1_num;
-jogador2.textContent = j2.nome
+jogador2.textContent = j2.nome;
 pontos_j2.textContent = 'Pontos: ' + pontos_j2_num;
 
 // MONTAR A 1a QUESTAO COMPLETA, para iniciar o Quiz
 // // Coloca o número da questão em azul
-numQuestao.textContent = q1.numQuestao
+numQuestao.textContent = q1.numQuestao;
 
 // // Coloca o parametro da questão 
-pergunta.textContent   = q1.pergunta
+pergunta.textContent   = q1.pergunta;
 
 // // Coloca o texto das alternativas
-a.textContent = q1.alternativaA
-b.textContent = q1.alternativaB
-c.textContent = q1.alternativaC
-d.textContent = q1.alternativaD
+a.textContent = q1.alternativaA;
+b.textContent = q1.alternativaB;
+c.textContent = q1.alternativaC;
+d.textContent = q1.alternativaD;
 
 // CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
-a.setAttribute('value', '1A')
-b.setAttribute('value', '1B')
-c.setAttribute('value', '1C')
-d.setAttribute('value', '1D')
+a.setAttribute('value', '1A');
+b.setAttribute('value', '1B');
+c.setAttribute('value', '1C');
+d.setAttribute('value', '1D');
 
 // PARA MONTAR AS PROXIMAS QUESTOES
 function proximaQuestao(nQuestao) {
@@ -219,26 +207,26 @@ function proximaQuestao(nQuestao) {
     jaClicouPraJogar = false;
 
     // coloca o número
-    numero.textContent = nQuestao
+    numero.textContent = nQuestao;
 
     // Coloca o número da questão em azul
-    numQuestao.textContent = questoes[nQuestao].numQuestao
+    numQuestao.textContent = questoes[nQuestao].numQuestao;
 
     // Coloca o parametro da questão
-    pergunta.textContent   = questoes[nQuestao].pergunta
+    pergunta.textContent   = questoes[nQuestao].pergunta;
 
     // Coloca o texto das alternativas
-    a.textContent = questoes[nQuestao].alternativaA
-    b.textContent = questoes[nQuestao].alternativaB
-    c.textContent = questoes[nQuestao].alternativaC
-    d.textContent = questoes[nQuestao].alternativaD
+    a.textContent = questoes[nQuestao].alternativaA;
+    b.textContent = questoes[nQuestao].alternativaB;
+    c.textContent = questoes[nQuestao].alternativaC;
+    d.textContent = questoes[nQuestao].alternativaD;
 
     // Configura o value da questão
-    a.setAttribute('value', nQuestao+'A')
-    b.setAttribute('value', nQuestao+'B')
-    c.setAttribute('value', nQuestao+'C')
+    a.setAttribute('value', nQuestao+'A');
+    b.setAttribute('value', nQuestao+'B');
+    c.setAttribute('value', nQuestao+'C');
     d.setAttribute('value', nQuestao+'D');
-}
+};
 
 // Verifica se já foi escolhida a resposta certa
 let jaClicouPraJogar = false;
@@ -247,8 +235,8 @@ function chamaAcertou(nQuestao, resposta) {
         verificarSeAcertou(nQuestao, resposta);
     } else {
         alert('Você não pode clicar de novo!');
-    }
-}
+    };
+};
 
 // Verifica se acertou a resposta
 let clicouPrajogar = false;
@@ -256,100 +244,101 @@ function verificarSeAcertou(nQuestao, resposta) {
 
     if (clicouPrajogar === true) {
         jaClicouPraJogar = true;
-        let numeroDaQuestao = nQuestao.value
-        console.log("Questão " + numeroDaQuestao)
+        let numeroDaQuestao = nQuestao.value;
+        console.log("Questão " + numeroDaQuestao);
     
-        let respostaEscolhida = resposta.textContent
-        //console.log("RespU " + respostaEscolhida)
+        // Pega resposta escolhida
+        let respostaEscolhida = resposta.textContent;
     
-        let certa = questoes[numeroDaQuestao].correta
-        //console.log("RespC " + certa)
+        // Pega resposta certa
+        let certa = questoes[numeroDaQuestao].correta;
     
         if(respostaEscolhida == certa) {
             if (clicou === 'jogador1'){
                 pontos_j1_num += 10;
                 pontos_j1.textContent = 'Pontos: ' + pontos_j1_num;
                 console.log('Pontos jogador 1 ' + pontos_j1_num);
-            }
+            };
             if (clicou === 'jogador2'){
                 pontos_j2_num += 10;
                 pontos_j2.textContent = 'Pontos: ' + pontos_j2_num;
                 console.log('Pontos jogador 2 ' + pontos_j2_num);
-            }
-            instrucoes.textContent = 'Acertou! Parabéns!'
+            };
+            instrucoes.textContent = 'Acertou! Parabéns!';
             
             setTimeout(function() {
                 instrucoes.textContent = `Leia a questão e clique na resposta correta`;
             }, 5000);
         } else {
-            instrucoes.textContent = 'Errou, preste mais atenção!'
+            instrucoes.textContent = 'Errou, preste mais atenção!';
 
             setTimeout(function() {
                 instrucoes.textContent = `Leia a questão e clique na resposta correta`;
             }, 5000);
-        }
+        };
     
         setTimeout(function() {
-            proxima = numeroDaQuestao+1
+            proxima = numeroDaQuestao+1;
     
             if(proxima > totalDeQuestoes) {
-                console.log('Fim do Jogo!')
-                fimDoJogo()
+                console.log('Fim do Jogo!');
+                fimDoJogo();
             } else {
-                proximaQuestao(proxima);
-                botaoJogar.disabled = false;
-    
-            }
-        }, 5000)
+                proximaQuestao(proxima);    
+            };
+        }, 5000);
     } else {
-        alert('Calma!!, quem clicar primeiro joga!')
-    }
+        alert('Calma!!, quem clicar primeiro joga!');
+    };
     
-}
+};
 
 function fimDoJogo() {
-    instrucoes.textContent = "Fim de Jogo!"
-    numQuestao.textContent = ""
+    instrucoes.textContent = "Fim de Jogo!";
+    numQuestao.textContent = "";
 
     if(pontos_j1_num > pontos_j2_num) {
-        let pont = ''
-        pontos_j2 == 0 ? pont = 'ponto' : pont = 'pontos'
+        let pont = '';
+        pontos_j1_num === 0 ? pont = 'ponto' : pont = 'pontos';
 
         let avisoFinal = `
             Parabéns ${j1.nome}\n
             Você conseguiu ${pontos_j1_num} ${pont}
-        `
+        `;
+
         aviso.textContent = avisoFinal;
-    }
+    };
 
     if (pontos_j2_num > pontos_j1_num) {
-        let pont = ''
-        pontos_j2 == 0 ? pont = 'ponto' : pont = 'pontos'
+        let pont = '';
+        pontos_j2_num === 0 ? pont = 'ponto' : pont = 'pontos';
 
         let avisoFinal = `
             Parabéns ${j2.nome}\n
             Você conseguiu ${pontos_j2_num} ${pont}
-        `
+        `;
+
         aviso.textContent = avisoFinal;
-    }
+    };
 
     if(pontos_j1_num === pontos_j2_num) {
         let avisoFinal = `
             Parabéns ${j1.nome} e ${j2.nome}\n
             Tivemos um empate!
-        `
+        `;
+
         aviso.textContent = avisoFinal;
-    }
+    };
 
-    a.textContent = ""
-    b.textContent = ""
-    c.textContent = ""
-    d.textContent = ""
+    a.textContent = "";
+    b.textContent = "";
+    c.textContent = "";
+    d.textContent = "";
 
-    a.setAttribute('value', '0')
-    b.setAttribute('value', '0')
-    c.setAttribute('value', '0')
-    d.setAttribute('value', '0')
+    a.setAttribute('value', '0');
+    b.setAttribute('value', '0');
+    c.setAttribute('value', '0');
+    d.setAttribute('value', '0');
 
     // OCULTAR O ARTICLE DA QUESTAO
     articleQuestoes.style.display = 'none';
@@ -359,7 +348,7 @@ function fimDoJogo() {
     // Faz com que a página recarregue
     setTimeout(function() {
         pontos_j1_num = 0;
-        pontos_j2_num = 0
+        pontos_j2_num = 0;
         location.reload();
-    }, 10000)
-}
+    }, 10000);
+};
